10df0c206f40d0a0644e1adbcff3fb03735192c8
Ricardo Alejandro Rojas Campos <ricardo.rojas.campos@gmail.com>
2025-03-12 11:43:06 -0500

    ok

--- src/etl/Transform.py
+++ src/etl/Transform.py
@@ -45,7 +45,7 @@ def procesar_excel(archivo_excel: str, nombre_sheet: str) -> pd.DataFrame:
         encabezados = ["MesInscrito", "MesAlta", "DiaClase", "Grupo", "DNI", "Nombres", "Apellidos", "Edad",
                     "C01", "C02", "C03", "C04", "C05", "C06", "C07", "C08", "C09", "C10", "C11", "C12"]
         df_final.columns = encabezados
-
+ 
         # Limpieza de datos
         rows_before = len(df_final)
         df_final = df_final.dropna(how="all")
